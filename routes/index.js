aefb45987713c2c2cf799340859c0da13f4f03fb
sl00p <lev@merka.us>
2022-08-18 21:04:46 -0700

    started routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./api/userRoutes');
-
+const userRoute = require('./api/user');
+const thoughtRoute = require('./api/thought')
 router.use('/users', userRoutes);
 
 router.use((req, res) => {
